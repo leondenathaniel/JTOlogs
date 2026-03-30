c1db95fbf99ec02aa531fcf9e010dc970a99c0f7
Egglorian <orb.devdrift@gmail.com>
2026-03-30 10:57:47 +0800

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import forms.JeepneyLogReg;
+
+public class main {
+
+    public static void main(String[] args) {
+        JeepneyLogReg jto = new JeepneyLogReg();
+        jto.setVisible(true);
+    }
+    
+}
